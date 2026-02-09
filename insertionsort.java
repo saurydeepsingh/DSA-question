84c0200ea064866abc0a6734a4d451d6a5f12a1a
saurydeep singh <saurydeepsingh231305@gmail.com>
2026-02-09 19:17:52 +0530

    Add files via upload

---
+++ insertionsort.java
@@ -0,0 +1,23 @@
+public class insertionsort {
+     public static void insertion(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            int curr=arr[i];
+            int prev=i-1;
+            while (prev>=0 && arr[prev] > curr) {
+                arr[prev+1]= arr[prev];
+                prev--;
+                
+            }
+            arr[prev+1]=curr;
+        }
+    }
+    public static void main(String[] args){
+        int arr[]={1,5,2,8,5,0,6};
+        insertion(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+    
+}
