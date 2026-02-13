63604dfc3b362398c1ff470e6d3adca20e1db90e
saurydeep singh <saurydeepsingh231305@gmail.com>
2026-02-14 00:05:02 +0530

    Add files via upload

---
+++ largestString.java
@@ -0,0 +1,16 @@
+public class largestString {
+    // largest of String
+
+    public static void main(String[] args){
+        String str[]={"apple","mango","banana"};
+        String largest=str[0];
+        for(int i=1;i<str.length;i++){
+            if(largest.compareTo(str[i])<0){
+                largest=str[i];
+            }
+        }
+        System.out.print(largest);
+    }
+}
+    
+
