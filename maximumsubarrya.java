4e90581b5c2b3578b6eb17f6210832bd7fb31c99
saurydeep singh <saurydeepsingh231305@gmail.com>
2026-02-13 22:33:44 +0530

    Add files via upload

--- maximumsubarrya.java
+++ maximumsubarrya.java
@@ -1,17 +1,21 @@
 public class maximumsubarrya {
-    public static int max(int arr[]){
+    public static int maxsunarrya(int arr[]){
         int sum=0;
+        int max=Integer.MIN_VALUE;
         for(int i=0;i<arr.length;i++){
             sum+=arr[i];
+            max=Math.max(max, sum);
+
+            if(sum<0){
+                sum=0;
+            }
         }
         return sum;
     }
     public static void main(String[] args){
-        int arr[]={1,3,5,7};
-        // max(arr);
-        // for(int i=0;i<arr.length;i++){
-            System.out.print(max(arr));
-        // }
+        int arr[]={5,4,-1,7,8};
+
+        System.out.print(maxsunarrya(arr));
+
     }
-    
 }
