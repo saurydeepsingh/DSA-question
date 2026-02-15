2c36f322fee0079b2d61b21a1ef5d5a79ad66783
saurydeep singh <saurydeepsingh231305@gmail.com>
2026-02-15 16:43:35 +0530

    Add files via upload

--- linearsearch.java
+++ linearsearch.java
@@ -1,17 +1,16 @@
 public class linearsearch {
-    public static void linear(int number[], int key){
-        for(int i=0;i<number.length;i++){
-            if(number[i]==key){
-                System.out.print(i);
+    public static void linearsearch(int arr[], int key){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==key){
+                System.out.println("the key is found at the index of"+" "+i);
+
             }
-            
         }
-        System.out.print("not number");
     }
     public static void main(String[] args){
-        int number[]={1,2,5,7,3};
-        int key=7;
-        linear(number, key);
+        int arr[]={1,2,3,4,5,6,7};
+        int key=4;
+        linearsearch(arr, key);
     }
     
 }
