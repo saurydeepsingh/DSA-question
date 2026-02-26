b28b0822ee96e04d1cf21402eaab700f6eb1c242
saurydeep singh <saurydeepsingh231305@gmail.com>
2026-02-26 11:29:35 +0530

    Add files via upload

---
+++ previouslong.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+public class previouslong{
+    public static void main(String[] args) {
+        int a[] = {4, 5, 2, 7, 9};
+        Stack<Integer> s = new Stack<>();
+
+        for (int i = 0; i < a.length; i++) {
+
+            while (!s.isEmpty() && s.peek() <= a[i]) {
+                s.pop();
+            }
+
+            if (s.isEmpty()) {
+                System.out.print(-1 + " ");
+            } else {
+                System.out.print(s.peek() + " ");
+            }
+
+            s.push(a[i]);
+        }
+    }
+}
