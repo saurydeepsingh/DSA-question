f8bbdfff2308006a921104f1dbcbf2e369e978b2
saurydeep singh <saurydeepsingh231305@gmail.com>
2026-03-17 19:27:50 +0530

    Add files via upload

--- largestofarray.java
+++ largestofarray.java
@@ -1,13 +1,27 @@
+// import java.util.Arrays;
+
+// public class largestofarray {
+//     public static int largestnumber(int arr[]){
+//         Arrays.sort(arr);
+//         return arr[arr.length-1];
+//     }
+//     public static void main(String[] args){
+//         int arr[]={1,4,7,9,4,6};
+//         System.out.print(largestnumber(arr));
+//     }
+    
+// }
+
 import java.util.Arrays;
 
-public class largestofarray {
-    public static int largestnumber(int arr[]){
+public class largestofarray{
+    public static int largestis(int arr[] , int key){
         Arrays.sort(arr);
-        return arr[arr.length-1];
+
+        return arr[arr.length-key];
     }
     public static void main(String[] args){
-        int arr[]={1,4,7,9,4,6};
-        System.out.print(largestnumber(arr));
+        int arr[]={1,3,5,7,9};
+       System.out.print( largestis(arr, 3));
     }
-    
 }
